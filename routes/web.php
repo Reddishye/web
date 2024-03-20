d134bcd1f16ec1aa9f12eb204b356625f0a65855
Reddishye <hugotorresmartinez09@outlook.es>
2024-03-20 23:59:15 +0100

    Add /projects

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,4 +15,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('projects', ProjectController::class);
 });
