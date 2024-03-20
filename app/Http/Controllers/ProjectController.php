b9ab5235e3b95ebc7c8d2c1a20e17316c754a038
Reddishye <hugotorresmartinez09@outlook.es>
2024-03-20 23:57:49 +0100

    Add ProjectController

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Projects;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Projects::all();
+        return view('projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'status' => 'required',
+            'authors' => 'required',
+        ]);
+
+        $authors = explode(',', $validatedData['authors']);
+        $validatedData['authors'] = $authors;
+
+        $project = Projects::create($validatedData);
+
+        return redirect()->route('projects.index')->with('success', 'Project created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Projects $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Projects $project)
+    {
+        return view('projects.edit', compact('project'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Projects $project)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'status' => 'required',
+            'authors' => 'required',
+        ]);
+
+        $authors = explode(',', $validatedData['authors']);
+        $validatedData['authors'] = $authors;
+
+        $project->update($validatedData);
+
+        return redirect()->route('projects.index')->with('success', 'Project updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Projects $project)
+    {
+        $project->delete();
+
+        return redirect()->route('projects.index')->with('success', 'Project deleted successfully');
+    }
+}
