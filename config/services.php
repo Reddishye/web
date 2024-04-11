d1fbc8d308d8bec899d1baaa614cdc57eb7ef2c8
Reddishye <hugotorresmartinez09@outlook.es>
2024-04-11 20:21:19 +0200

    Add discord settings to services.php

--- config/services.php
+++ config/services.php
@@ -31,4 +31,10 @@ return [
         ],
     ],
 
+    'discord' => [
+        'client_id' => env('DISCORD_CLIENT_ID'),
+        'client_secret' => env('DISCORD_CLIENT_SECRET'),
+        'redirect' => env('DISCORD_REDIRECT_URI'),
+    ],
+
 ];
