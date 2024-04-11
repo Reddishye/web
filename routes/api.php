9f071b0f852c72258d186b677d459814d9fa4914
Reddishye <hugotorresmartinez09@outlook.es>
2024-04-11 20:14:15 +0200

    Add checklicense route to api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/checklicense/{licenseKey}', [App\Http\Controllers\Api\LicenseController::class, 'checkLicense']);
