b039cebb8539991a620d0271a9242220010f76cd
Reddishye <hugotorresmartinez09@outlook.es>
2024-04-11 20:20:44 +0200

    Require admin permission to access admin pages Add discord unlink route Add an alias to call analytics page in other files Add customer view routes like: - Move dashboard to customer routes - Add discord auth and callback routes - Add licenses route

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,34 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\UserViewController;
 use App\Http\Controllers\LinksManager;
 use App\Http\Controllers\LinksViewer;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserViewController;
+use App\Http\Controllers\LicenseController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserViewController::class, 'index'])->name('home');
 Route::get('/links/{path?}', [LinksViewer::class, 'redirect'])->where('path', '.*')->name('links.redirect');
-Route::redirect('/dashboard', '/admin/dashboard');
 
 Route::prefix('admin')->middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+    'permission:admin',
+])->group(function () {
+    Route::resource('projects', ProjectController::class);
+
+    Route::resource('links', LinksManager::class);
+
+    Route::resource('users', UserController::class);
+    Route::put('/users/{user}/discord/unlink', [UserController::class, 'unlinkDiscord'])->name('users.discord.unlink');
+
+    Route::get('analytics', function () {
+    })->name('analytics');
+});
+
+Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
@@ -20,9 +37,7 @@ Route::prefix('admin')->middleware([
         return view('dashboard');
     })->name('dashboard');
 
-    Route::resource('projects', ProjectController::class);
-
-    Route::resource('links', LinksManager::class);
-
-    Route::resource('users', UserController::class);
+    Route::get('/auth/discord', [App\Http\Controllers\DiscordController::class, 'redirectToProvider'])->name('auth.discord');
+    Route::get('/auth/discord/callback', [App\Http\Controllers\DiscordController::class, 'handleProviderCallback']);
+    Route::resource('licenses', LicenseController::class);
 });
