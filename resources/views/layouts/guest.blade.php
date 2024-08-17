d613234dbf3c320b7e83402cb4ea09f09f6bbcc0
Reddishye <hugotorresmartinez09@outlook.es>
2024-08-17 21:15:41 +0200

    Add Discord meta tags and extra fonts

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,9 @@
     <meta charset="UTF-8">
     <meta name="description" content="Discover the portfolio of Hugo Torres, an expert developer in Laravel, Minecraft plugins, Discord bots, and Minecraft server configuration.">
 
+    <!-- Discord -->
+    <meta property="og:site_name" content="Hugo Torres, Developer">
+
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://redactado.es/">
@@ -22,8 +25,15 @@
     <meta name="twitter:url" content="https://redactado.es/">
     <meta name="twitter:title" content="Hugo Torres, Developer">
     <meta name="twitter:description" content="Discover the portfolio of Hugo Torres, an expert developer in Laravel, Minecraft plugins, Discord bots, and Minecraft server configuration.">
+
+    <!-- Extra Fonts -->
+    <link href="{{ asset('fonts/Inconsolata-Regular.woff2') }}" rel="font/woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wdth,wght@50..200,200..900&display=swap" rel="stylesheet">
+
 </head>
-    <body x-data="themeSwitcher()" :class="{ 'dark': switchOn }" class="antialiased text-gray-900 transition duration-200 bg-gray-100 dark:bg-gray-900 dark:text-white">
+    <body class="antialiased text-gray-900 transition duration-200 bg-gray-100 dark:bg-black dark:text-white">
         <div class="font-sans">
             {{ $slot }}
         </div>
